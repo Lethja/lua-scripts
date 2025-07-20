da7a1831bc8cb4976781d6bb1cfa477131217dc9
Jason Lethbridge <lethbridgejason@gmail.com>
2025-07-20 15:13:01 +1000

    Make absoluetly sure the FATSTAT unit test gives mformat a zeroed-out file to format and that it returns non-zero when an a mismatch occurs.

--- test/FATSTAT/test.sh
+++ test/FATSTAT/test.sh
@@ -4,7 +4,8 @@ INTERP=${INTERP:-lua}
 TEST="FATSTAT"
 SCRIPT="FATSTAT.LUA"
 
-mformat -i test.ima -C -f 160
+dd if=/dev/zero of=test.ima bs=512 count=320 status=none
+mformat -i test.ima -f 160
 CHASH=$(sha256sum test.ima)
 # Insert junk data to wipe
 dd if=/dev/random of=test.ima bs=512 count=2 seek=300 status=none
@@ -13,4 +14,4 @@ if [ "$CHASH" = "$RHASH" ]; then die; fi
 $INTERP $SCRIPT -z test.ima
 ZHASH=$(sha256sum test.ima)
 rm test.ima
-if [ "$CHASH" = "$ZHASH" ]; then echo "$TEST: PASS"; else echo "$TEST: FAIL"; fi
+if [ "$CHASH" = "$ZHASH" ]; then echo "$TEST: PASS"; else echo "$TEST: FAIL"; exit 1; fi
