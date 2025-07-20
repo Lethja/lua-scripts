c571f5d57be8465b58fec3a6b7dbb7f8ed505178
Jason Lethbridge <lethbridgejason@gmail.com>
2025-07-20 11:48:28 +1000

    Added unit test for FATSTAT.LUA

---
+++ test/FATSTAT/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+INTERP=${INTERP:-lua}
+TEST="FATSTAT"
+SCRIPT="FATSTAT.LUA"
+
+mformat -i test.ima -C -f 160
+CHASH=$(sha256sum test.ima)
+# Insert junk data to wipe
+dd if=/dev/random of=test.ima bs=512 count=2 seek=300 status=none
+RHASH=$(sha256sum test.ima)
+if [ "$CHASH" = "$RHASH" ]; then die; fi
+$INTERP $SCRIPT -z test.ima
+ZHASH=$(sha256sum test.ima)
+rm test.ima
+if [ "$CHASH" = "$ZHASH" ]; then echo "$TEST: PASS"; else echo "$TEST: FAIL"; fi
