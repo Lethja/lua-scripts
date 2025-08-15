7ace558ed8747f398d32e612e71ab11b794348d5
Jason Lethbridge <lethbridgejason@gmail.com>
2025-08-15 13:12:04 +1000

    Added padding around unit test printouts in PATCH.LUA unit tests.

--- test/PATCH/test.sh
+++ test/PATCH/test.sh
@@ -8,29 +8,30 @@ test_case() {
   mkdir -p result
   cp "case$1"/* result
   cd "result"
+  echo "## CASE $1: START  ##"
   diff -u a.txt b.txt > "diff"
   if [ -f "c.txt" ] && [ -f "d.txt" ]; then diff -u c.txt d.txt >> "diff"; fi
   $INTERP $SCRIPT diff
   if [ -f "c.txt" ] && [ -f "d.txt" ]; then
     if { set +e; cmp -s a.txt b.txt; } && { set +e; cmp -s c.txt d.txt; }; then
-      printf "\tCASE %s: PASS\n" "$1"
+      printf "## CASE %s: PASS   ##\n" "$1"
     else
-      printf "\tCASE %s: FAIL\n" "$1"
+      printf "## CASE %s: FAIL   ##\n" "$1"
       FAIL=$((FAIL + 1))
     fi
   else
     if { set +e; cmp -s a.txt b.txt; }; then
-      printf "\tCASE %s: PASS\n" "$1"
+      printf "## CASE %s: PASS   ##\n" "$1"
     else
-      printf "\tCASE %s: FAIL\n" "$1"
+      printf "## CASE %s: FAIL   ##\n" "$1"
       FAIL=$((FAIL + 1))
     fi
   fi
   cd ..
   rm -R result
 }
-echo "PATCH:"
+echo "## PATCH:          ##"
 for i in $(seq -w 1 21); do test_case "$i"; done
 echo ""
-if [ $FAIL -gt 0 ]; then echo "PATCH: $FAIL FAIL"; exit 1; fi
-echo "PATCH: ALL PASS"
+if [ $FAIL -gt 0 ]; then echo "## PATCH: $FAIL FAIL ##"; exit 1; fi
+echo "## PATCH: ALL PASS ##"
