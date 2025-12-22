39f15dd6e900965f03da342d57ad1e40937f5c2a
Jason Lethbridge <lethbridgejason@gmail.com>
2025-12-22 18:22:48 +1100

    Update unzip unit test to check that stored files also extract correctly.

--- meta/test/unzip/test.sh
+++ meta/test/unzip/test.sh
@@ -2,18 +2,38 @@
 set -e
 INTERP=${INTERP:-lua}
 SCRIPT="UNZIP.LUA"
+R=0
 
-# Compress a binary file
+# Inflate test
+TEST="$SCRIPT store"
+
+# Store a binary file in a zip
 cp `which $INTERP` .
 SRC="$(sha256sum ./$INTERP)"
 if [ -f test.zip ]; then rm "test.zip"; fi
-zip test.zip "./$INTERP"
+zip test.zip -Z store "./$INTERP"
 
 # Extract and compare
 rm "./$INTERP"
 $INTERP $SCRIPT test.zip
 DST="$(sha256sum ./$INTERP)"
-if [ "$SRC" = "$DST" ]; then echo "$SCRIPT": PASS; else echo "$SCRIPT: FAIL"; fi
-
-# Clean up
+if [ "$SRC" = "$DST" ]; then echo "$TEST": PASS; else echo "$TEST: FAIL"; R=$((R+1)); fi
 rm "./$INTERP" test.zip
+
+# Inflate test
+TEST="$SCRIPT inflate"
+
+# Compress a binary file with deflate
+cp `which $INTERP` .
+SRC="$(sha256sum ./$INTERP)"
+if [ -f test.zip ]; then rm "test.zip"; fi
+zip test.zip -Z deflate "./$INTERP"
+
+# Extract and compare
+rm "./$INTERP"
+$INTERP $SCRIPT test.zip
+DST="$(sha256sum ./$INTERP)"
+if [ "$SRC" = "$DST" ]; then echo "$TEST": PASS; else echo "$TEST: FAIL"; R=$((R+1)); fi
+rm "./$INTERP" test.zip
+
+exit $R
