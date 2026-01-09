40ec345d51a9bf11070f9466a6361cfc71248bb7
Jason Lethbridge <lethbridgejason@gmail.com>
2026-01-10 02:34:21 +1100

    Added extension unit test to unzip.

--- meta/test/unzip/test.sh
+++ meta/test/unzip/test.sh
@@ -22,6 +22,24 @@ DST="$(sha256sum ./$DATA)"
 if [ "$SRC" = "$DST" ]; then echo "$TEST": PASS; else echo "$TEST: FAIL"; R=$((R+1)); fi
 rm "./$DATA" test.zip
 
+# Extension test
+TEST="$SCRIPT extension"
+DATA=$(readlink -f $(which $INTERP))
+
+# Store a binary file in a zip
+cp $DATA .
+DATA=$(basename $DATA)
+SRC="$(sha256sum ./$DATA)"
+if [ -f test.zip ]; then rm "test.zip"; fi
+zip test.zip -Z store "./$DATA"
+
+# Extract and compare
+rm "./$DATA"
+$INTERP $SCRIPT test
+DST="$(sha256sum ./$DATA)"
+if [ "$SRC" = "$DST" ]; then echo "$TEST": PASS; else echo "$TEST: FAIL"; R=$((R+1)); fi
+rm "./$DATA" test.zip
+
 # Inflate test
 TEST="$SCRIPT inflate"
 DATA=$(readlink -f $(which $INTERP))
